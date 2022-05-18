646e25077034cf859087fd1adc6a5b09cf2d5b78
Rene Ortega <reneortega2468@gmail.com>
2022-05-18 19:58:30 -0400

    api routes done.

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+// TODO: Pass in objects like, user-routes
+const {
+  createdThought, 
+    updatedThought,
+      addReaction,
+        getAllThoughts,
+          getThoughtById,
+            deleteThought,
+              deleteReaction,
+} = require('../../controllers/thought-controller')
+
+// TODO: Pass in objects to your routes
+
+router.route('/').get(getAllThoughts).post(createdThought);
+
+  router.route('/:id').get(getThoughtById).put(updatedThought).delete(deleteThought);
+  
+      router.route('/:thoughtId/reactions/').post(addReaction).delete(deleteReaction);
+
+      module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,9 +1,26 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+// const { User } = require('../../models');
 
-router.post('/', (req, res) => {
-  User.create(req.body)
-    .then(data => res.json(data));
-});
+const { 
+  getAllUsers, 
+    addFriend,
+      removeFriend,
+        getUserById,
+          deleteUser,
+          updateUser,
+            createUser,
+} = require('../../controllers/user-controller');
+// TODO: Pass in all objects to routes. PUT GET DELETE POST
+
+router.route('/').get(getAllUsers).post(createUser);
+
+router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+// router.post('/', (req, res) => {
+//   User.create(req.body)
+//     .then(data => res.json(data));
+// });
 
 module.exports = router;
